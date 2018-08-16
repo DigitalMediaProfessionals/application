fc8d84fe2c576f9ca8e8892e504917b27ab0fb75
Alexey Kazantsev <alexey.kazantsev@dmprof.com>
2018-08-16 12:35:09 +0900

    Started implementin CDMP_Network for new driver

---
+++ common/include/dmp_network.h
@@ -0,0 +1,293 @@
+/*
+ *  Copyright 2018 Digital Media Professionals Inc.
+
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+
+ *      http://www.apache.org/licenses/LICENSE-2.0
+
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+/*
+ * @brief Helpers for feed forward neural network specification.
+ */
+#pragma once
+
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+
+#include <stdint.h>
+
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+
+#include "stats.h"
+#include "dmp_dv.h"
+#include "dmp_dv_cmdraw_v0.h"
+
+
+/// @brief Layer type enumeration.
+enum layer_type {
+  LT_INPUT,
+  LT_CONV,
+  LT_FC,
+  LT_FLATTEN,
+  LT_CONCAT,
+  LT_COPY_CONCAT,
+  LT_SOFTMAX,
+  LT_CUSTOM,
+};
+
+
+/// @brief Forward reference for layer specification.
+struct fpga_layer;
+
+
+/// @brief Callback.
+typedef void (*run_custom_callback_proc)(fpga_layer &layer, void *custom_param);
+
+
+/// @brief Layer specification.
+struct fpga_layer {  // TODO: refactor it.
+  layer_type    type;
+  void *        hw_conf;
+  void *        addr_cpu_input;
+  void *        addr_cpu_output;
+  unsigned long addr_offset_input;
+  unsigned long addr_offset_output;
+  unsigned long output_size;
+  int           input_dim[3];
+  int           input_dim_size;
+  int           output_dim[3];
+  int           output_dim_size;
+  bool          is_output;
+  bool          is_f32_output;
+  bool          is_input_hw_layout;
+  union {
+    struct {
+      int   softmax_axis;
+    };
+    struct {
+      int input_layer_num;
+      fpga_layer **input_layers;
+    };
+    struct {
+      run_custom_callback_proc custom_proc_ptr;
+      void *custom_param;
+    };
+  };
+  std::string name;         // layer name
+  dmp_dv_cmdlist *cmdlist;  // command list containing this layer, can be NULL
+  int cmdlist_pos;          // position of this layer in the command list
+  int cmdlist_size;         // size of the command list
+};
+
+
+/// @brief Base helper class for feed forward neural network specification.
+class CDMP_Network {
+ public:
+  /// @brief Constructor.
+  CDMP_Network() {
+    iprint_ = 0;
+    num_layers_ = 0;
+    num_output_layers_ = 0;
+    num_conv_layers_ = 0;
+    num_fc_layers_ = 0;
+    weights_size_ = 0;
+    io_size_ = 0;
+
+    ctx_ = NULL;
+    weights_mem_ = NULL;
+    io_mem_ = NULL;
+
+    last_conv_usec_ = 0;
+    last_fc_usec_ = 0;
+    last_cpu_usec_ = 0;
+  };
+
+  /// @brief Destructor.
+  virtual ~CDMP_Network() {
+    ReleaseResources();
+  };
+
+  /// @brief Returns reference to output layer definition.
+  inline std::vector<fpga_layer*>& get_output_layers() {
+    return output_layers_;
+  }
+
+  /// @brief Returns reference to the specific layer definition.
+  fpga_layer& get_layer(int i);
+
+  /// @brief Returns total number of layers.
+  inline int get_total_layer_count() const {
+    return num_layers_;
+  }
+
+  /// @brief Returns number of output layers.
+  inline int get_output_layer_count() const {
+    return num_output_layers_;
+  }
+
+  /// @brief Returns number of convolutional layers.
+  inline int get_convolution_layer_count() const {
+    return num_conv_layers_;
+  }
+
+  /// @brief Returns number of fully connected layers.
+  inline int get_fc_layer_count() const {
+    return num_fc_layers_;
+  }
+
+  /// @brief Initializes the network.
+  virtual bool Initialize() = 0;
+
+  /// @brief Reserves memory for weights and input/output.
+  bool ReserveMemory();
+
+  /// @brief Loads packed weights from file.
+  bool LoadWeights(const std::string& filename);
+
+  /// @brief Runs the network.
+  /// @param sync_io_mem If true, do Device<->CPU input/output memory synchronization at the beginning and at the end of this function.
+  bool RunNetwork(bool sync_io_mem=true);
+
+  /// @brief Returns address of the input buffer of the first layer.
+  void *get_network_input_addr_cpu();
+
+  /// @brief Copies output data to the provided buffer in single-precision float format.
+  void get_final_output(std::vector<float>& output, int i_output = 0);
+
+  /// @brief Returns last time spent for executing convolutional layers in microseconds.
+  inline int get_last_conv_usec() const {
+    return last_conv_usec_;
+  }
+
+  /// @brief Returns last time spent for fully connected layers in microseconds.
+  inline int get_last_fc_usec() const {
+    return last_fc_usec_;
+  }
+
+  /// @brief Returns last time spent for CPU layers in microseconds.
+  inline int get_cpu_usec() const {
+    return last_cpu_usec_;
+  }
+
+  /// @brief Sets verbosity level.
+  inline void verbose(int level) {
+    iprint_ = level;
+  }
+
+ protected:
+  /// @brief Creates required number of command lists and assigns "cmdlist*" fileds in layers_.
+  /// @details Must be called at the end of Initialize() in derived classes.
+  bool GenerateCommandLists();
+
+  /// @brief Number of layers.
+  int num_layers_;
+
+  /// @brief Number of output layers.
+  int num_output_layers_;
+
+  /// @brief Number of convolutional layers.
+  int num_conv_layers_;
+
+  /// @brief Number of fully connected layers.
+  int num_fc_layers_;
+
+  /// @brief Size of weights buffer in bytes.
+  size_t weights_size_;
+
+  /// @brief Size of input/output buffer in bytes.
+  size_t io_size_;
+
+  /// @brief Layers description in order of execution.
+  std::vector<fpga_layer> layers_;
+
+  /// @brief Pointers to output layers.
+  std::vector<fpga_layer*> output_layers_;
+
+  /// @brief Context.
+  dmp_dv_context *ctx_;
+
+  /// @brief Memory allocation for weights.
+  dmp_dv_mem *weights_mem_;
+
+  /// @brief Memory allocation for input/output.
+  dmp_dv_mem *io_mem_;
+
+  /// @brief Command lists for executing different sets of layers.
+  std::vector<dmp_dv_cmdlist*> cmd_lists_;
+
+  /// @brief Last time spent on all convolutional layers in microseconds.
+  int last_conv_usec_;
+
+  /// @brief Last time spent on all fully connected layers in microseconds.
+  int last_fc_usec_;
+
+  /// @brief Last time spent on all layers executed on CPU in microseconds.
+  int last_cpu_usec_;
+
+ private:
+  /// @brief Releases held resources.
+  void ReleaseResources() {
+    for (auto it = cmd_lists_.rbegin(); it != cmd_lists_.rend(); ++it) {
+      dmp_dv_cmdlist_release(*it);
+    }
+    cmd_lists_.clear();
+    dmp_dv_mem_release(io_mem_);
+    dmp_dv_mem_release(weights_mem_);
+    dmp_dv_context_release(ctx_);
+
+    output_layers_.clear();
+    layers_.clear();
+
+    num_layers_ = 0;
+    num_output_layers_ = 0;
+    num_conv_layers_ = 0;
+    num_fc_layers_ = 0;
+    weights_size_ = 0;
+    io_size_ = 0;
+
+    ctx_ = NULL;
+    weights_mem_ = NULL;
+    io_mem_ = NULL;
+
+    last_conv_usec_ = 0;
+    last_fc_usec_ = 0;
+    last_cpu_usec_ = 0;
+  }
+
+  /// @brief Verbosity level.
+  bool iprint_;
+
+  /// @brief Dummy layer to return on error.
+  static fpga_layer err_layer_;
+};
+
+
+/// @brief Fills provided vector with the input data for the specified layer.
+void get_layer_input(fpga_layer& layer, std::vector<float>& layer_input);
+
+
+/// @brief Fills provided vector with the output data for the specified layer.
+/// @param layer Layer to retrieve output from.
+/// @param layer_output Vector to put requested data into.
+/// @param is_output_hw_layout Whether data should be put in HW-specific layout (WHC8) or in Caffe-style CHW.
+void put_layer_output(fpga_layer& layer, std::vector<float>& layer_output, bool is_output_hw_layout = false);
---
+++ common/src/dmp_network.cpp
@@ -0,0 +1,474 @@
+/*
+*------------------------------------------------------------
+* Copyright(c) 2017 by Digital Media Professionals Inc.
+* All rights reserved.
+*------------------------------------------------------------
+* This source code was generated using DMP-DV700 tools
+* Digital Media Professionals Inc.
+*------------------------------------------------------------
+*/
+
+#include <stdarg.h>
+
+#include "dmp_network.h"
+
+
+/// @brief Dummy layer to return on error (instantiation).
+fpga_layer CDMP_Network::err_layer_;
+
+
+#define LOG(...) fprintf(stdout, __VA_ARGS__); fflush(stdout);
+#define ERR(...) fprintf(stderr, __VA_ARGS__); fflush(stderr);
+
+
+fpga_layer& CDMP_Network::get_layer(int i) {
+  if ((i < 0) || (i >= (int)layers_.size())) {
+    LOG("Requested layer index is out of bounds: %d\n", i);
+    return err_layer_;
+  }
+  return layers_[i];
+}
+
+
+bool CDMP_Network::ReserveMemory() {
+  if (!ctx_) {
+    ERR("Context is NULL, Initialize() must be called and succeed first\n");
+    return false;
+  }
+  if ((weights_mem_) || (io_mem_)) {
+    ERR("Double call of ReserveMemory()\n");
+    return false;
+  }
+
+  weights_mem_ = dmp_dv_mem_alloc(ctx_, weights_size_);
+  if (!weights_mem_) {
+    ERR("Failed to allocate %zu bytes for DV accelerator: %s\n",
+        weights_size_, dmp_dv_get_last_error_message());
+    return false;
+  }
+  if (iprint_ > 0) {
+    LOG("Allocated %zu bytes of memory for weights (%zu requested)\n",
+        dmp_dv_mem_get_size(weights_mem_), weights_size_);
+  }
+
+  io_mem_ = dmp_dv_mem_alloc(ctx_, io_size_);
+  if (!io_mem_) {
+    ERR("Failed to allocate %zu bytes for DV accelerator: %s\n",
+        io_size_, dmp_dv_get_last_error_message());
+    return false;
+  }
+  if (iprint_ > 0) {
+    LOG("Allocated %zu bytes of memory for input/output (%zu requested)\n",
+        dmp_dv_mem_get_size(io_mem_), io_size_);
+  }
+
+  return true;
+}
+
+
+bool CDMP_Network::LoadWeights(const std::string& filename) {
+  if (!weights_mem_) {
+    ERR("Weights buffer is NULL, ReserveMemory() must be called and succeed first\n");
+    return false;
+  }
+
+  FILE *fin = fopen(filename.c_str(), "rb");
+  if (!fin) {
+    ERR("Failed to open weights file: %s\n", filename.c_str());
+    return false;
+  }
+
+  uint8_t *buf = dmp_dv_mem_map(weights_mem_);
+  if (!buf) {
+    ERR("Failed to map %zu bytes of weights memory into application's virtual memory: %s\n",
+        dmp_dv_mem_get_size(weights_mem_), dmp_dv_get_last_error_message());
+    fclose(fin);
+    return false;
+  }
+  if (dmp_dv_mem_sync_start(weights_mem_, 0, 1)) {
+    ERR("Failed to start CPU->Device memory synchronization for %zu bytes of weights memory: %s\n",
+        dmp_dv_mem_get_size(weights_mem_), dmp_dv_get_last_error_message());
+    fclose(fin);
+    return false;
+  }
+  size_t n = fread(buf, 1, weights_size_, fin);
+  if (n != weights_size_) {
+    ERR("Incomplete read of weights from %s: read %zu bytes instead of %zu\n",
+        filename.c_str(), n, weights_size_);
+    fclose(fin);
+    return false;
+  }
+  char c;
+  if ((fread(&c, 1, 1, fin) != 0) || (!feof(fin))) {
+    ERR("File with weights %s is larger than computed weights size %zu\n",
+        filename.c_str(), weights_size_);
+    fclose(fin);
+    return false;
+  }
+  fclose(fin);
+  dmp_dv_mem_unmap(weights_mem_);
+
+  if (iprint_ > 0) {
+    LOG("Loaded %zu bytes for weights from %s\n",
+        weights_size_, filename.c_str());
+  }
+
+  return true;
+}
+
+
+/// @brief Reorders channels.
+static void remap(uint16_t* __restrict src, uint16_t* __restrict dst, int x_size, int y_size, int channel_size) {
+  for (int y = 0; y < y_size; ++y) {
+    for (int x = 0; x < x_size; ++x) {
+      for (int i = 0; i < channel_size; i += 8) {
+        const int copy_size = channel_size - i > 8 ? 8 : channel_size - i;
+        memcpy(dst + (y * x_size + x) * channel_size + i,
+               src + i * (x_size * y_size) + (x * y_size + y) * copy_size,
+               copy_size * sizeof(uint16_t));
+      }
+    }
+  }
+}
+
+
+/// @brief Reorders channels.
+static void remap_hw(uint16_t* __restrict src, uint16_t* __restrict dst, int x_size, int y_size, int channel_size) {
+  for (int y = 0; y < y_size; ++y) {
+    for (int x = 0; x < x_size; ++x) {
+      for (int i = 0; i < channel_size; i += 8) {
+        const int copy_size = (channel_size - i > 8 ? 8 : channel_size - i);
+        memcpy(dst + i * (x_size * y_size) + (x * y_size + y) * copy_size,
+               src + (y * x_size + x) * channel_size + i,
+               copy_size * sizeof(uint16_t));
+      }
+    }
+  }
+}
+
+
+/// @brief Converts array of half precision floats to single precision.
+static void fp16_to_float(float* __restrict dst, const __fp16* __restrict src, int n) {
+  for (int i = 0; i < n; ++i) {
+    dst[i] = (float)src[i];
+  }
+}
+
+
+/// @brief Converts array of single precision floats to half precision.
+static void float_to_fp16(__fp16* __restrict dst, const float* __restrict src, int n) {
+  for (int i = 0; i < n; ++i) {
+    dst[i] = (__fp16)src[i];
+  }
+}
+
+
+/// @brief Runs softmax operation on CPU.
+static void run_softmax(fpga_layer& layer, int softmax_axis) {
+  void *src_buffer = layer.addr_cpu_input;
+  void *dst_buffer = layer.addr_cpu_output;
+
+  if (layer.is_input_hw_layout) {
+    uint16_t *src = (uint16_t*)src_buffer;
+    uint16_t *dst = (uint16_t*)dst_buffer;
+    int x_size = layer.input_dim[0];
+    int y_size = layer.input_dim[1];
+    int channel_size = layer.input_dim[2];
+    dst += x_size * y_size * channel_size;
+    remap(src, dst, x_size, y_size, channel_size);
+    src_buffer = dst;
+  }
+
+  int tensor_size = 1, group_size, axis_size, remain_size;
+  int axis_stride = 1, group_stride;
+  for (int i = 0; i < layer.input_dim_size; i++) {
+    tensor_size *= layer.input_dim[i];
+    if (i > softmax_axis) {
+      axis_stride *= layer.input_dim[i];
+    }
+    axis_size = layer.input_dim[softmax_axis];
+    remain_size = axis_stride;
+    group_size = tensor_size / axis_size / remain_size;
+    group_stride = axis_size * axis_stride;
+    fp16_to_float((float*)dst_buffer, (__fp16*)src_buffer, tensor_size);
+
+    for (int j = 0; j < group_size; ++j) {
+      for (int k = 0; k < remain_size; ++k) {
+        float *f = (float*)dst_buffer + (j * group_stride + k);
+        float fmax = f[0];
+        for (int i = 1; i < axis_size; ++i) {
+          fmax = std::max(fmax, f[i * axis_stride]);
+        }
+        for (int i = 0; i < axis_size; ++i) {
+          f[i * axis_stride] -= fmax;
+        }
+        float e_sum = 0;
+        for (int i = 0; i < axis_size; ++i) {
+          float d = std::exp(f[i * axis_stride]);
+          f[i * axis_stride] = d;
+          e_sum += d;
+        }
+        if(std::fabs(e_sum) < 1e-6f) {
+          ERR("SoftMax implementaion error, the sum of exp is too low: %.6e\n", e_sum);
+        }
+
+        float inv_e_sum = 1.0f / e_sum;
+        for (int i = 0; i < axis_size; i++) {
+          f[i * axis_stride] *= inv_e_sum;
+        }
+      }
+    }
+  }
+}
+
+
+/// @brief Executes flatten operation.
+static void run_flatten(fpga_layer &layer) {
+  if (!layer.is_input_hw_layout) {
+    return;
+  }
+  uint16_t *src = (uint16_t*)layer.addr_cpu_input;
+  uint16_t *dst = (uint16_t*)layer.addr_cpu_output;
+  int x_size = layer.input_dim[0];
+  int y_size = layer.input_dim[1];
+  int channel_size = layer.input_dim[2];
+  remap(src, dst, x_size, y_size, channel_size);
+}
+
+
+/// @brief Executes concatenation via memory copy.
+static void run_copy_concat(fpga_layer& layer, int input_layer_num, fpga_layer **input_layers) {
+  const int chunk_size = 8;
+  uint16_t *dst = (uint16_t*)layer.addr_cpu_output;
+  const int x_size = layer.output_dim[0];
+  const int y_size = layer.output_dim[1];
+  const int dst_channel_size = layer.output_dim[2];
+  const int chunk_stride = x_size * y_size * chunk_size;
+  int dst_copied_size = 0;
+  for (int i = 0; i < input_layer_num; ++i) {
+    uint16_t *src = (uint16_t*)input_layers[i]->addr_cpu_output;
+    const int src_channel_size = input_layers[i]->output_dim[2];
+    int src_copied_size = 0;
+    while (src_copied_size < src_channel_size) {
+      int dst_copy_size = chunk_size - (dst_copied_size % chunk_size);
+      if (dst_copy_size > dst_channel_size - dst_copied_size) {
+        dst_copy_size = dst_channel_size - dst_copied_size;
+      }
+      int src_copy_size = chunk_size - (src_copied_size % chunk_size);
+      if (src_copy_size > src_channel_size - src_copied_size) {
+        src_copy_size = src_channel_size - src_copied_size;
+      }
+      int copy_size = (dst_copy_size < src_copy_size ? dst_copy_size : src_copy_size);
+      uint16_t *dst_copy = dst + (dst_copied_size / chunk_size) * chunk_stride + (dst_copied_size % chunk_size);
+      uint16_t *src_copy = src + (src_copied_size / chunk_size) * chunk_stride + (src_copied_size % chunk_size);
+      const int dst_stride = (dst_channel_size / chunk_size > dst_copied_size / chunk_size ? chunk_size : dst_channel_size % chunk_size);
+      const int src_stride = (src_channel_size / chunk_size > src_copied_size / chunk_size ? chunk_size : src_channel_size % chunk_size);
+      for (int j = 0; j < x_size * y_size; ++j) {
+        memcpy(dst_copy, src_copy, sizeof(uint16_t) * copy_size);
+        dst_copy += dst_stride;
+        src_copy += src_stride;
+      }
+      dst_copied_size += copy_size;
+      src_copied_size += copy_size;
+    }
+  }
+}
+
+
+bool CDMP_Network::RunNetwork(bool sync_io_mem) {
+  TimeInterval dt;
+  int64_t exec_id = -1;
+  dmp_dv_cmdlist *cmdlist = NULL;
+  int conv_usec = 0;
+  int fc_usec = 0;
+  int cpu_usec = 0;
+
+  if (sync_io_mem) {
+    dmp_dv_mem_sync_end(io_mem_);
+  }
+
+  for (auto it = layers_.begin(); it != layers_.end(); ++it) {
+    if (it->cmdlist) {
+      // Execute command list on first command
+      if (it->cmdlist_pos == 0) {
+        if (exec_id >= 0) {
+          ERR("Incorrect layers order detected: layer name=%d cmdlist_pos=%d cmdlist_size=%d, but exec_id is already set\n",
+              it->name.c_str(), it->cmdlist_pos, it->cmdlist_size);
+          return false;
+        }
+        dt.reset();  // start time measurement
+        exec_id = dmp_dv_cmdlist_exec(it->cmdlist);
+        if (exec_id < 0) {
+          ERR("Could not execute command list for layer %s: %s\n",
+              it->name.c_str(), dmp_dv_get_last_error_message());
+          return false;
+        }
+        cmdlist = it->cmdlist;  // remember last executed command list
+      }
+
+      // Wait for command list completion on last command
+      if (it->cmdlist_pos == it->cmdlist_size - 1) {
+        if (exec_id < 0) {
+          ERR("Incorrect layers order detected: layer name=%d cmdlist_pos=%d cmdlist_size=%d, but exec_id is not set\n",
+              it->name.c_str(), it->cmdlist_pos, it->cmdlist_size);
+          return false;
+        }
+        if (dmp_dv_cmdlist_wait(cmdlist, exec_id)) {
+          ERR("Wait for command completion failed, issued on layer name=%d: %s\n",
+              it->name.c_str(), dmp_dv_get_last_error_message());
+          return false;
+        }
+        const int usec = dt.get_us();
+        switch (it->type) {
+          case LT_CONV:
+            conv_usec += usec;
+            break;
+          case LT_FC:
+            fc_usec += usec;
+            break;
+          default:
+            ERR("Possible implementation error on line %d of file %s: layer name=%d cmdlist_pos=%d cmdlist_size=%d\n",
+                __LINE__, __FILE__, it->name.c_str(), it->cmdlist_pos, it->cmdlist_size);
+            return false;
+        }
+        exec_id = -1;
+        cmdlist = NULL;
+      }
+      if (it->cmdlist_pos >= it->cmdlist_size) {
+        ERR("Possible implementation error on line %d of file %s: layer name=%d cmdlist_pos=%d cmdlist_size=%d\n",
+            __LINE__, __FILE__, it->name.c_str(), it->cmdlist_pos, it->cmdlist_size);
+        return false;
+      }
+    }
+    else if (exec_id >= 0) {
+      ERR("Incorrect layers order detected: layer name=%d cmdlist_pos=%d cmdlist_size=%d, but exec_id is already set\n",
+          it->name.c_str(), it->cmdlist_pos, it->cmdlist_size);
+      return false;
+    }
+    switch (it->type) {
+      case LT_CONV:
+      case LT_FC:
+        if (!it->cmdlist) {
+          ERR("Command list for layer %s is NULL, Initialize() must be called and succeed first\n",
+              it->name.c_str());
+          return false;
+        }
+        break;
+      case LT_SOFTMAX:
+        dt.reset();
+        run_softmax(*it, it->softmax_axis);
+        cpu_usec += dt.get_us();
+        break;
+      case LT_FLATTEN:
+        dt.reset();
+        run_flatten(*it);
+        cpu_usec += dt.get_us();
+        break;
+      case LT_COPY_CONCAT:
+        dt.reset();
+        run_copy_concat(*it, it->input_layer_num, it->input_layers);
+        cpu_usec += dt.get_us();
+        break;
+      case LT_CUSTOM:
+        dt.reset();
+        (*(it->custom_proc_ptr))(*it, it->custom_param);
+        cpu_usec += dt.get_us();
+        break;
+      case LT_INPUT:
+      case LT_CONCAT:
+        // Layer does nothing by design
+        break;
+    }
+  }
+
+  last_conv_usec_ = conv_usec;
+  last_fc_usec_ = fc_usec;
+  last_cpu_usec_ = cpu_usec;
+
+  if (sync_io_mem) {
+    dmp_dv_mem_sync_start(io_mem_, 1, 1);
+  }
+
+  return true;
+}
+
+
+void* CDMP_Network::get_network_input_addr_cpu() {
+  return layers_[0].addr_cpu_input;
+}
+
+
+void CDMP_Network::get_final_output(std::vector<float>& out, int i_output) {
+  int output_size = output_layers_[i_output]->output_size;
+  if (output_layers_[i_output]->is_f32_output) {
+    output_size >>= 2;
+  }
+  else {
+    output_size >>= 1;
+  }
+
+  if((int)out.size() != output_size) {
+    out.resize(output_size);
+  }
+
+  if (output_layers_[i_output]->is_f32_output) {
+    memcpy(out.data(), output_layers_[i_output]->addr_cpu_output, output_size << 2);
+  }
+  else {
+    fp16_to_float(out.data(), (__fp16*)output_layers_[i_output]->addr_cpu_output, output_size);
+  }
+}
+
+
+void get_layer_input(fpga_layer &layer, std::vector<float> &layer_input) {
+    int input_size = 1;
+    for (int i = 0; i < layer.input_dim_size; ++i) {
+      input_size *= layer.input_dim[i];
+    }
+
+    if(layer_input.size() != input_size) {
+      layer_input.resize(input_size);
+    }
+
+    if (layer.is_input_hw_layout) {
+      uint16_t *src = (uint16_t*)layer.addr_cpu_input;
+      uint16_t *dst = (uint16_t*)&layer_input.front();
+      int x_size = layer.input_dim[0];
+      int y_size = layer.input_dim[1];
+      int channel_size = layer.input_dim[2];
+      dst += input_size;
+      remap(src, dst, x_size, y_size, channel_size);
+      fp16_to_float((float*)&layer_input.front(), (__fp16*)dst, input_size);
+    }
+    else {
+      fp16_to_float((float*)&layer_input.front(), (__fp16*)layer.addr_cpu_input, input_size);
+    }
+}
+
+
+void put_layer_output(fpga_layer& layer, std::vector<float>& layer_output, bool is_output_hw_layout) {
+  int output_size = 1;
+  for (int i = 0; i < layer.output_dim_size; ++i) {
+    output_size *= layer.output_dim[i];
+  }
+
+  if (!is_output_hw_layout) {
+    memcpy(layer.addr_cpu_output, (void*)(&layer_output.front()), layer.output_size);
+  }
+  else {
+    float *src = (float*)&layer_output.front();
+    __fp16 *dst = (__fp16*)layer.addr_cpu_output;
+    int x_size = layer.output_dim[0];
+    int y_size = layer.output_dim[1];
+    int channel_size = layer.output_dim[2];
+    dst += output_size;
+    float_to_fp16(dst + output_size, src, output_size);
+    remap_hw(dst + output_size, dst, x_size, y_size, channel_size);
+  }
+}
+
+
+bool CDMP_Network::GenerateCommandLists() {
+  // TODO: implement.
+  return true;
+}
