068a6bb1669748c537e479b04c1c717cedf11d12
YuyaKobayashi <yuya.kobayashi@dmprof.com>
2018-11-26 11:54:47 +0900

    update API

--- common/include/dmp_ipu.h
+++ common/include/dmp_ipu.h
@@ -31,7 +31,8 @@ class DMPIPUConfig {
     /// @param width Width. This can be 0 if a texture buffer is given from CDMP_Network object.
     /// @param heigh Height. This can be 0 if a texture buffer is given from CDMP_Network object.
     /// @param cnv_param see struct dmp_dv_cmdraw_ipu_v0.cnv_param . If given, this length must be 3.
-    /// @param ncolor_lut number of colors for LUT. This must be smaller than 33.
+    /// @param ncolor_lut number of colors for LUT and length of argument `lut`.
+    //                    This must be smaller than 33.
     /// @detail each parametes corresponds to members of struct dmp_dv_cmdraw_ipu_v0
     ///         width  <-> tex_width
     ///         height <-> tex_height
@@ -45,14 +46,14 @@ class DMPIPUConfig {
                       uint8_t cnv_type = DMP_DV_CNV_FP16_SUB, const uint8_t *cnv_param = nullptr,
                       uint8_t ncolor_lut = 0, uint32_t *lut = nullptr);
  
-    /// @brief configure to use texture buffer
+    /// @brief configure to use read buffer
     /// @param width Width. This must be the same as width of write buffer.
     /// @param height Height. This must be the same as height of write buffer.
     /// @param stride Stride. If 0 is given, set the same value as width.
     /// @return 0 on success, -1 otherwise
     int ConfigUseRD(uint8_t format, uint16_t width, uint16_t height, int32_t stride = 0);
 
-    /// @brief configure of write buffer
+    /// @brief configure write buffer
     /// @param width Width. This can be 0 if a write buffer is given from CDMP_Network object. 
     ///                               If not 0, this must be the same as width of read buffer.
     /// @param heigh Height. This can be 0 if a write buffer is given from CDMP_Network object.
@@ -64,14 +65,14 @@ class DMPIPUConfig {
     /// @brief configure to use constant alpha value
     void ConfigUseConstAlpha(uint8_t alpha);
 
-    /// @brief configure to use BLF
+    /// @brief configure to use bilinear filter
     void ConfigUseBLF();
 
     /// @brief clear configuration
     void Clear();
 
     /// @brief getter for cmd
-    struct dmp_dv_cmdraw_ipu_v0 *get_dmp_dv_cmdraw_ipu_v0();
+    struct dmp_dv_cmdraw_ipu_v0 *get_rawcmd();
 
   private:
     struct dmp_dv_cmdraw_ipu_v0 cmd;
@@ -83,17 +84,21 @@ class DMPIPU {
 
     /// @brief Destructor.
     virtual ~DMPIPU() {
-      ReleaseResources();
+      Clear();
     };
 
     /// @brief initializer
+    /// @detail This does not accociate IPU with Network
     int Initialize(DMPIPUConfig &initializer);
 
-    /// @brief initializer for preprocessing network input
+    /// @brief initialize IPU for preprocessing network input
+    /// @param initializer You need not configure write-buffer. 
+    ///                     When write-buffer is configured, the format must be DMP_DV_RGBFP16.
     /// @detail an input buffer of net is assigned to a write buffer of IPU
+    /// @return 0 on success, -2 if invalid initializer is passed, -1 otherwise
     int InitializePreproc(DMPIPUConfig &initializer, const CDMP_Network &net);
 
-    /// @brief initializer for postprocessing network output
+    /// @brief initialize IPU for postprocessing network output
     /// @detail an output buffer of net is assigned to a texture buffer of IPU
     int InitializePostproc(DMPIPUConfig &initializer, const CDMP_Network &net, int i_output = 0);
 
@@ -115,22 +120,31 @@ class DMPIPU {
     /// @brief return if this IPU is connected to network output
     bool is_postprocessing() const;
 
-  private:
-    void Clear();
-    void ReleaseResources();
+    /// @brief return if this is already initialized
+    bool is_initialized() const;
 
-    int InitializeCmdlist(dmp_dv_cmdraw_ipu_v0 *cmd);
+  private:
+    void Clear(bool release_resources=true);
+
+    int InitializeCmdlist(struct dmp_dv_cmdraw_ipu_v0 *cmd);
+    int AllocAndMapEachMem(struct dmp_dv_cmdraw_ipu_v0 *cmd);
 
     dmp_dv_context ctx;
     dmp_dv_cmdlist cmdlist;
 
+    /// rd and wr do not consider stride when calculating offs
     struct dmp_dv_buf tex;
     struct dmp_dv_buf rd;
     struct dmp_dv_buf wr;
+
+    /// These are a start address of mapped memory, not of memory region for each buffer.
+    /// These variables may have the same address.
     uint8_t *tex_map;
     uint8_t *rd_map;
     uint8_t *wr_map;
 
     int i_output;
     const CDMP_Network *net;
+
+    bool initialized;
 };
